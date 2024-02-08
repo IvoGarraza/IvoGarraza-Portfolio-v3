c428ef043f6af4dc59a3c4c24097d836770571b1
Ivo Garraza <ivogarraza@gmail.com>
2024-02-08 10:36:41 -0300

    arreglo skillsmodal

--- src/components/SkillModal.jsx
+++ src/components/SkillModal.jsx
@@ -18,11 +18,11 @@ const variantsCard = {
 let transitionDuration = 0.3;
 let delayDuration = 0.0;
 
-const SkillModal = ({isOpen,setIsOpen,handleClick}) => {
+const SkillModal = ({ isOpen, setIsOpen, handleClick }) => {
   const { t } = useTranslation("global");
-  const [stack, setStack] = useState('all')
+  const [stack, setStack] = useState("all");
 
-/*   const handleClick = () => {
+  /*   const handleClick = () => {
     setIsOpen(false);
   }; */
 
@@ -33,50 +33,75 @@ const SkillModal = ({isOpen,setIsOpen,handleClick}) => {
       <div className="flex sm:flex-row flex-col items-center justify-between w-full ">
         <div className="flex flex-row">
           <div className="sm:flex grid grid-cols-2 grid-rows-2 sm:flex-col items-center text-white text-lg font-bold">
-            <div className={`flex flex-row ${stack == 'frontend'?'bg-orange-400': 'bg-slate-400'} transition-all rounded-full px-2 my-1 hover:bg-orange-400 cursor-pointer`}
-            onClick={()=>setStack('frontend')}>
+            <div
+              className={`flex flex-row ${
+                stack == "frontend" ? "bg-orange-400" : "bg-slate-400"
+              } transition-all rounded-full px-2 my-1 hover:bg-orange-400 cursor-pointer`}
+              onClick={() => setStack("frontend")}
+            >
               <ComputerDesktopIcon className="w-6 text-black fill-white"></ComputerDesktopIcon>
               <span>Frontend</span>
             </div>
-            <div className={`flex flex-row ${stack == 'backend' ? 'bg-orange-400':'bg-slate-400'} transition-all rounded-full px-2 my-1 hover:bg-orange-400 cursor-pointer`}
-            onClick={()=>setStack('backend')}>
+            <div
+              className={`flex flex-row ${
+                stack == "backend" ? "bg-orange-400" : "bg-slate-400"
+              } transition-all rounded-full px-2 my-1 hover:bg-orange-400 cursor-pointer`}
+              onClick={() => setStack("backend")}
+            >
               <CogIcon className="w-6 text-black fill-white"></CogIcon>
               <span>Backend</span>
             </div>
-            <div className={`flex flex-row ${stack == 'database' ? 'bg-orange-400':'bg-slate-400'} transition-all hover:bg-orange-400 cursor-pointer rounded-full px-2 my-1`}
-            onClick={()=>setStack('database')}>
+            <div
+              className={`flex flex-row ${
+                stack == "database" ? "bg-orange-400" : "bg-slate-400"
+              } transition-all hover:bg-orange-400 cursor-pointer rounded-full px-2 my-1`}
+              onClick={() => setStack("database")}
+            >
               <CircleStackIcon className="w-6 text-black fill-white"></CircleStackIcon>
               <span>Database</span>
             </div>
-            <div className={`flex flex-row ${stack == 'tools' ? 'bg-orange-400':'bg-slate-400'} transition-all hover:bg-orange-400 cursor-pointer bg-slate-400 rounded-full px-2 my-1`}
-            onClick={()=>setStack('tools')}>
+            <div
+              className={`flex flex-row ${
+                stack == "tools" ? "bg-orange-400" : "bg-slate-400"
+              } transition-all hover:bg-orange-400 cursor-pointer bg-slate-400 rounded-full px-2 my-1`}
+              onClick={() => setStack("tools")}
+            >
               <WrenchScrewdriverIcon className="w-6 text-black fill-white"></WrenchScrewdriverIcon>
               <span>Tools</span>
             </div>
           </div>
         </div>
-        <div className="grid sm:grid-cols-5 grid-cols-3 w-full place-items-center gap-4">
-        <div className="grid sm:grid-cols-5 grid-cols-3 w-full place-items-center gap-4">
-          {logos.map((logo, index) => {
-            const row = Math.floor(index / 3);
-            const delay = (index % 3) * 0.05 + row * 0.1;
-            const transition = { duration: 0.05, delay: delay };
-            return (
-              <motion.div
-                variants={variantsCard}
-                whileHover="uno"
-                initial="hidden"
-                animate="aparition"
-                transition={transition}
-                className=" p-6 h-32 flex flex-col items-center justify-between text-center"
-              >
-                <img className={`sm:w-16 w-10 ${stack == logo.stack || stack == 'all' ? 'grayscale-0':'grayscale'}`} src={logo.img} />
-                <span className="font-medium sm:text-lg text-xs">{logo.name}</span>
-              </motion.div>
-            );
-          })}
-        </div>
-      </div>
+        
+          <div className="grid sm:grid-cols-5 grid-cols-3 w-full place-items-center gap-4">
+            {logos.map((logo, index) => {
+              const row = Math.floor(index / 3);
+              const delay = (index % 3) * 0.05 + row * 0.1;
+              const transition = { duration: 0.05, delay: delay };
+              return (
+                <motion.div
+                  variants={variantsCard}
+                  whileHover="uno"
+                  initial="hidden"
+                  animate="aparition"
+                  transition={transition}
+                  className=" p-6 h-32 flex flex-col items-center justify-between text-center"
+                >
+                  <img
+                    className={`sm:w-16 w-10 ${
+                      stack == logo.stack || stack == "all"
+                        ? "grayscale-0"
+                        : "grayscale"
+                    }`}
+                    src={logo.img}
+                  />
+                  <span className="font-medium sm:text-lg text-xs">
+                    {logo.name}
+                  </span>
+                </motion.div>
+              );
+            })}
+          </div>
+        
       </div>
     </div>
   );
